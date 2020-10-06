7de7461009f89a04d3ea6a3d629d8205cbcd4f33
Baiqu-an <qh19445@bristol.ac.uk>
2020-10-06 17:16:43 +0100

    Q2 and 3

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -55,4 +55,10 @@ func ask(s score, question question) score {
 
 func main() {
 	// TODO: Write a quiz program
+	s := score(0)
+	listQuest := questions()
+	for _, quest := range listQuest {
+		s = ask(s, quest)
+	}
+	fmt.Println("final score:", s)
 }
--- sequences/sequences.go
+++ sequences/sequences.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func addOne(a int) int {
 	return a + 1
 }
@@ -8,18 +10,34 @@ func square(a int) int {
 	return a * a
 }
 
-func double(slice []int) {
+func double(slice []int) []int {
 	slice = append(slice, slice...)
+	return slice
 }
 
 func mapSlice(f func(a int) int, slice []int) {
-
+	for i, num := range slice {
+		slice[i] = f(num)
+	}
 }
 
-func mapArray(f func(a int) int, array [3]int) {
-
+func mapArray(f func(a int) int, array [3]int) [3]int {
+	var newArray [3]int
+	for i := 0; i < 3; i++ {
+		newArray[i] = f(array[i])
+	}
+	return newArray
 }
 
 func main() {
-
+	intsSlice := []int{1, 2, 3, 4, 5}
+	newSlice := intsSlice[1:3]
+	mapSlice(addOne, intsSlice)
+	// mapSlice(square, newSlice)
+	intsSlice = double(intsSlice)
+	fmt.Println(newSlice)
+	fmt.Println(intsSlice)
+	intsArray := [3]int{1, 2, 3}
+	intsArray = mapArray(addOne, intsArray)
+	fmt.Println(intsArray)
 }
